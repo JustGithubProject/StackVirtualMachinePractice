338a1b42216ae1eba648795ae6523cd859d8ef15
JustGithubProject <Kropivadanil@gmail.com>
2024-07-21 16:10:45 +0300

    Defined stack-vm.h file

---
+++ stack-vm.cpp
@@ -0,0 +1,25 @@
+#include "stack-vm.h"
+
+/*
+    Instruction format
+    header: 2 bits
+    data: 30 bits
+
+    header format:
+    0 => positive integer
+    1 => primitive instruction
+    2 => negative integer
+    3 => undefined
+*/
+
+// functions
+StackVM::StackVM()
+{
+    memory.reserve(1000000);
+}
+
+i32 StackVM::getType(i32 instruction)
+{
+    i32 type = 0xc0000000;
+    type = (type & instruction) >> 30;
+}
