ffcf7a329fc246e32a58cd9b4bfe1cb478ecda3f
JustGithubProject <Kropivadanil@gmail.com>
2024-07-21 19:06:21 +0300

    Defined lexer.h file

---
+++ sasm/lexer.h
@@ -0,0 +1,33 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include <iostream>
+#include <vector>
+
+// Type definitions
+typedef uint8_t byte;
+typedef std::vector<std::string> strings;
+
+enum State : byte 
+{
+    START,
+    READCHAR,
+    READBLOCK,
+    SKIP,
+    DUMP,
+    COMMENT,
+    END
+}
+
+class Lexer
+{
+private:
+    bool myIsSpace(char c);
+    bool isSpecial(char c);
+    bool isGroup(char c);
+    char endChar, begChar;
+public:
+    strings lex(std::string s);
+};
+
+#endif
