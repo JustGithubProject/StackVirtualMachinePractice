26e2368b48d69c5e6cf6af432baed0f4623e2f30
JustGithubProject <Kropivadanil@gmail.com>
2024-07-21 16:17:23 +0300

    Defined implementation of getData, fetch, decode

--- stack-vm.cpp
+++ stack-vm.cpp
@@ -22,4 +22,22 @@ i32 StackVM::getType(i32 instruction)
 {
     i32 type = 0xc0000000;
     type = (type & instruction) >> 30;
+    return type;
+}
+
+i32 StackVM::getData(i32 instruction)
+{
+    i32 data = 0x3fffffff;
+    data = data & instruction;
+    return data;
+}
+
+void StackVM::fetch()
+{
+    pc++;
+}
+
+void StackVM::decode()
+{
+    typ = getType(memory[pc]);
 }
